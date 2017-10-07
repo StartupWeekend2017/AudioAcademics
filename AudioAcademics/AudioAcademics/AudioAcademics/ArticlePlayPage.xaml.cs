eb399ce399c24210d60cdcd3e20c5cc1a5755589
Pearse Hutson <pearse.2008@gmail.com>
2017-10-07 11:25:23 -0500

    Init Again

---
+++ AudioAcademics/AudioAcademics/AudioAcademics/ArticlePlayPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudibleAcademics.Models;
+using Plugin.MediaManager;
+using Plugin.MediaManager.Abstractions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AudibleAcademics
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ArticlePlayPage : ContentPage
+    {
+        private Article _article;
+        private IMediaManager PlaybackController => CrossMediaManager.Current;
+
+        public ArticlePlayPage(Article article)
+        {
+            InitializeComponent();
+
+            _article = article;
+
+            PlaybackController.PlayingChanged += (sender, e) =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    ArticleProgressBar.Progress = e.Progress;
+                });
+            };
+        }
+
+        protected override void OnAppearing()
+        {
+
+        }
+
+        private void ToggleArticle1(object sender, EventArgs e)
+        {
+            PlaybackController.PlaybackController.Stop();
+            PlaybackController.Play("https://phoodfridge.blob.core.windows.net/audibleacademics/4637.mp3");
+        }
+
+        private void ToggleArticle2(object sender, EventArgs e)
+        {
+            PlaybackController.PlaybackController.Stop();
+            PlaybackController.Play("https://phoodfridge.blob.core.windows.net/audibleacademics/95.mp3");
+        }
+
+        private void ToggleArticle3(object sender, EventArgs e)
+        {
+            PlaybackController.PlaybackController.Stop();
+            PlaybackController.Play("https://phoodfridge.blob.core.windows.net/audibleacademics/142.mp3");
+        }
+        private void Stop_OnClicked(object sender, EventArgs e)
+        {
+            PlaybackController.PlaybackController.Stop(); 
+        }
+
+        private void Start_OnClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AudioAcademics/AudioAcademics/AudioAcademics/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.MediaManager;
+using Plugin.MediaManager.Abstractions;
+
+namespace AudioAcademics
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_onClick(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ArticleList());
+        }
+    }
+}
